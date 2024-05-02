14efb1c8e4721c0f58dfa5e54313f8417da8c9bc
Petris Rodrigo Fernandes <petrisrf@gmail.com>
2024-05-02 19:04:29 -0300

    Cucumber test index page

--- features/product_creation.feature
+++ features/product_creation.feature
@@ -3,6 +3,15 @@ Feature: Create Product
   As an user
   I want to create and manage products
 
+  Scenario: List Products
+    Given There are no products stored
+    When I visit /products
+    Then I should have a successful response
+    And I should receive a json response
+    And JSON response should have the key products
+    And products key should be empty
+
+
   Scenario: Create Valid Product
     When I send data to product endpoint
     Then I should have a successful response
--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+Given('There are no products stored') do
+  Product.delete_all
+end
+
 When('I send data to product endpoint') do
   post '/products', { product: { name: 'product', description: 'description' } }
+end
+
+When(/^I visit (.*)$/) do |uri|
+  get uri
 end
 
 Then('I should have a successful response') do
@@ -12,9 +20,21 @@ Then('I should receive a json response') do
   expect { JSON.parse last_response.body }.to_not raise_error
 end
 
+Then(/^JSON response should have the key (.*)$/) do |key|
+  json_response = JSON.parse(last_response.body)
+
+  expect(json_response).to have_key key
+end
+
 Then('Product data should be in response') do
   product = JSON.parse(last_response.body)['product']
 
   expect(product['name']).to eq 'product'
   expect(product['description']).to eq 'description'
+end
+
+Then(/^(.*) key should be empty$/) do |key|
+  field = JSON.parse(last_response.body)[key]
+
+  expect(field).to be_empty
 end
